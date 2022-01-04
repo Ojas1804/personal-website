7f8b8ed73c476ade32081efba82a65a7468f5230
OJAS <ojas.srivastava.2001@gmail.com>
2022-01-04 15:26:36 +0530

    new animations

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function toggleMobileMenu(menu)
 {
     menu.classList.toggle('open');
 }
+
+
+(function() {
+    window.addEventListener('load', () => {
+        AOS.init({
+          duration: 800,
+          easing: 'ease-in-out',
+          once: true,
+          mirror: false
+        })
+      });
+    
+    })()
